91be227d1c61d8fec4a54276a30fd60ccb0a0bcf
Steffano Malaga <smalagadev@gmail.com>
2019-06-15 13:17:40 -0400

    File paths corrected

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,34 +1,40 @@
+const path = require('path');
+
 module.exports = function(app){
   app.get('/', function(req, res){
     res.send('Sweat-it up and running.');
   });
 
   app.get('/login', function(req, res){
-    res.send(`Please log in.`);
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
-  app.get('/menu', function(req, res){
-    res.send(`Main menu`);
+  app.get('/main', function(req, res){
+    res.sendFile(path.join(__dirname, "../public/main.html"));
   });
 
-  app.get('/progress', function(req, res){
-    res.send(`View previous workouts`);
+  app.get('/options', function(req, res){
+    res.sendFile(path.join(__dirname, "../public/options.html"));
   });
 
-  app.get('/goals', function(req, res){
-    res.send(`View your goals`);
-  });
-
-  app.get('/browse', function(req, res){
-    res.send(`Browse`);
-  });
-
-  app.get('/session', function(req, res){
-    res.send(`Start workout`);
-  });
-
-  app.get('/settings', function(req, res){
-    res.send(`app and profile settings`);
-  });
+  // app.get('/progress', function(req, res){
+  //   res.send(`View previous workouts`);
+  // });
+  //
+  // app.get('/goals', function(req, res){
+  //   res.send(`View your goals`);
+  // });
+  //
+  // app.get('/browse', function(req, res){
+  //   res.send(`Browse`);
+  // });
+  //
+  // app.get('/session', function(req, res){
+  //   res.send(`Start workout`);
+  // });
+  //
+  // app.get('/settings', function(req, res){
+  //   res.send(`app and profile settings`);
+  // });
 
 }
