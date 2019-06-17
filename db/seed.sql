6b4046ebccee5b0abf29c53ce571dcc23cc308ef
Steffano Malaga <smalagadev@gmail.com>
2019-06-17 19:38:52 -0400

    Exercises types updated

--- db/seed.sql
+++ db/seed.sql
@@ -4,8 +4,8 @@ INSERT INTO users(pass, first_name, last_name) VALUES('1234', 'Test', 'Dummy');
 INSERT INTO users(pass, first_name, last_name) VALUES('1234', 'Demo', 'Dummy');
 
 -- Exercise see
-INSERT INTO exercises(name, description, type) VALUES('Push-ups', 'Calisthenic exercise focusing on the upper body.', 'pectoral,triceps');
+INSERT INTO exercises(name, description, type) VALUES('Push-ups', 'Calisthenic exercise focusing on the upper body.', 'upper-body');
 
-INSERT INTO exercises(name, description, type) VALUES('Sit-ups', 'Calisthenic exercise focusing on the core.', 'abdomen');
+INSERT INTO exercises(name, description, type) VALUES('Sit-ups', 'Calisthenic exercise focusing on the core.', 'upper-body');
 
-INSERT INTO exercises(name, description, type) VALUES('Squats', 'Calisthenic exercise focusing on the lower body.', 'quadriceps,glutes');
+INSERT INTO exercises(name, description, type) VALUES('Squats', 'Calisthenic exercise focusing on the lower body.', 'lower-body');
